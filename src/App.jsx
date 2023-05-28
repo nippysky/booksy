6067670b1da7f90be7ded1bb4f90c1d43d997a51
Chukwudubem Osegbe <lawrenceosegbe97@gmail.com>
2023-05-28 09:19:28 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl text-blue-500 font-bold underline text-center">
+      We are Ready To Start Coding. Nigeria | Kenya
+    </h1>
+  );
+}
+
+export default App;
